7e64040bb78950aa28c336105ee405e58180568c
Seyd Havens <seyd.havens@gmail.com>
2018-08-20 19:05:06 -0700

    Updated header for mobile screens

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-let $ = require('jquery');
-import Person from "./modules/Person";
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person{
-    payTaxes(){
-        console.log(this.name + " now owes $0 in taxes.");
-    }
-}
-
-alert("abc123");
-
-let john = new Person("John Doe", "blue");
-john.greet();
-let jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+let mobileMenu = new MobileMenu()
